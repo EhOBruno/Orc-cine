697bd1bdeffac9b0887f219ecad474a571d84e39
Ana Clara <abarbosaborges17@gmail.com>
2023-03-27 17:17:29 -0300

    começando formulario de login

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,9 +1,26 @@
 import React from 'react'
+import './Login.css'
 
 function Login() {
   return (
     <div>
-      <h1>Página de login</h1>
+      <h2>Entrar</h2>
+      <form>
+        <input
+        type='text'
+        placeholder='escreva seu usuário'
+        name='text'
+        />
+        <input
+        type='password'
+        placeholder='escreva sua senha'
+        name='password'
+        />
+        <input
+        type='submit'
+        placeholder='Entrar'
+        />
+      </form>
     </div>
   )
 }
