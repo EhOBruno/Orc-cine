652451abc9e7ee6dba63b7f716e2a10c97b5a34f
Ana Clara <abarbosaborges17@gmail.com>
2023-03-30 14:02:01 -0300

    tirando conflitos

--- src/index.js
+++ src/index.js
@@ -4,22 +4,8 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home/Home';
 import Popular from './pages/Popular/Popular';
 import Detalhes from './pages/Detalhes/Detalhes';
-<<<<<<< HEAD
-import { SessionContextProvider } from './contexts/SessionContext';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <SessionContextProvider>
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='/popular' element={<Popular/>}/>
-        <Route path='/detalhes/:id' element={<Detalhes/>}/>
-      </Routes>
-    </BrowserRouter>
-  </SessionContextProvider>
-=======
 import Busca from './pages/Busca/Busca';
+import Login from './pages/Login/Login';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -28,10 +14,8 @@ root.render(
       <Route path='/' element={<Home/>}/>
       <Route path='/popular' element={<Popular/>}/>
       <Route path='/login' element={<Login/>}/>
-      <Route path='/cadastro' element={<Cadastro/>}/>
       <Route path='/detalhes/:id' element={<Detalhes/>}/>
       <Route path='/busca' element={<Busca/>}/>
     </Routes>
   </BrowserRouter>
->>>>>>> main
 );
