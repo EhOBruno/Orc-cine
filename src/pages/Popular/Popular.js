59c47eddce48adbf840766cc995c5aa356290a78
Ana Clara <abarbosaborges17@gmail.com>
2023-03-31 18:02:30 -0300

    arrumando conflitos

--- src/pages/Popular/Popular.js
+++ src/pages/Popular/Popular.js
@@ -4,6 +4,7 @@ import NavBar from '../../components/NavBar'
 import { APIkey } from '../../key'
 import './Popular.css'
 import { FaStar } from "react-icons/fa";
+import FilterMovies from "../../components/FilterMovies"
 
 function Popular() {
 
@@ -27,21 +28,26 @@ function Popular() {
       return(window.location.href = `https://www.themoviedb.org/authenticate/${token}?redirect_to=http://localhost:3000/popular`)
     }
     
+    const [filterTextValue, setFilterTextValue] = useState('en')
+
     function onFilterValueSelected(filterValue){
       setFilterTextValue(filterValue)
     }
+
     const filteredMovies = movies.filter((movies) => {
           if(filterTextValue===movies.original_language){
             return movies.original_language===filterTextValue
           }
     }
     )
+
   return (
     <div>
         <header>
             <nav>
                 <ul className='navbar'>
                     <NavBar/>
+                    <FilterMovies onFilterValueSelected={onFilterValueSelected} />
                     <li><button onClick={aproveToken}>Entrar</button></li>
                 </ul>
             </nav>
@@ -50,7 +56,7 @@ function Popular() {
       <h1>Os Filmes Mais Populares!</h1>
 
       <ul className='popular-movies'>
-        {movies.map(movie => {
+        {filteredMovies.map(movie => {
             return(
                 <li className='movie' key={movie.id}>
                     <Link to={`/detalhes/${movie.id}`}><img src={`${imagePath}${movie.poster_path}`} alt='poster'/></Link>
