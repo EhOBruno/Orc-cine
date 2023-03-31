d8f8abee8ebcd22e723d2589fae833431ef9903c
Ana Clara <abarbosaborges17@gmail.com>
2023-03-31 00:26:07 -0300

    começando a autenticação

--- src/contexts/SessionContext.js
+++ src/contexts/SessionContext.js
@@ -1,25 +1,37 @@
 import { createContext , useState } from 'react'
-
-const baseUser = {
-    sessionId: null,
-    username: null
-}
+import { APIkey } from '../key'
 
 export const SessionContext = createContext(baseUser)
 
 export function SessionContextProvider(props){
-    const [user, setUser] = useState(baseUser)
 
-    let sessionId = localStorage.getItem('sessionId')
-    if (sessionId !== null){
-        setUser({
-            sessionId: sessionId,
-            username: localStorage.getItem('username')
-        })
+    const [token, setToken] = useState('')
+    const [sessionId, setSessionId] = useState('')
+
+    useEffect(() => {
+        fetch(`https://api.themoviedb.org/3/authentication/token/new?api_key=${APIkey}`)
+            .then(response => response.json())
+            .then(data => setToken(data.request_token))
+            localStorage.setItem('request_token', token)
+    })
+
+    function aproveToken() {
+        return(window.location.href = `https://www.themoviedb.org/authenticate/${localStorage.getItem('request_token')}?redirect_to=http://localhost:3000/login`)
+    }
+
+    function createSession() {
+        const init = {
+            method: 'POST',
+            body: JSON.stringfy(localStorage.getItem(request_token, 'request_token'))
+        }
+        fetch(`https://api.themoviedb.org/3/authentication/session/new?api_key=${APIkey}`, init)
+            .then(response => response.json())
+            .then(data => setSessionId(data.session_id))
+            localStorage.setItem('session_id', sessionId)
     }
 
     return (
-        <SessionContext.Provider value = {{user, setUser}}>
+        <SessionContext.Provider value={{aproveToken, createSession}}>
             {props.children}
         </SessionContext.Provider>
     )
